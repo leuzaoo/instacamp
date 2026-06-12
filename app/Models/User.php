bdbcfa80cdfc94d499c20c434ce0f8e2d85d55ad
leuzaoo <dev.lcostaoliveira@gmail.com>
2026-06-12 09:22:00 +0100

    Initialize Laravel project with essential configuration files and structure. Added .editorconfig, .env.example, .gitattributes, .gitignore, .npmrc, and basic Laravel application files including controllers, models, and configuration settings. Set up package management with composer.json and package.json for dependencies.

---
+++ app/Models/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    protected $connection = 'mongodb';
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'username',
+        'email',
+        'password',
+        'profile_image',
+        'bio',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
